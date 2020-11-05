a1582ce5618f73432635e6547a093864c0197307
Eduardomorera <morera.e@gmail.com>
2020-11-05 14:21:52 +0100

    //Unidad 1 - actividad 2 - ejercicio 2 - 1.2.2

---
+++ hola mundo/src/Saludo.java
@@ -0,0 +1,17 @@
+//Unidad 1 - actividad 2 - ejercicio 2 - 1.2.2
+
+public class Saludo {
+public static void main(String[] args) {
+System.out.println("Bienvenidos a la asignatura de Programación");
+}
+}
+
+
+/*
+*
+*Analizar el error e intentar arreglarlo, una vez arreglado, volver a compilar el código fuente.
+*System.out.println("Bienvenidos a la asignatura de Programación"); 
+*el error estaba en esta línea faltaba la " final del texto a mostrar
+*/
+
+
