010a4fa958d864b32cbbe9c4913a905d76c8e518
Eduardomorera <morera.e@gmail.com>
2020-11-05 14:31:24 +0100

    //Unidad 1 - actividad 2 - ejercicio 3 - 1.2.3

---
+++ hola mundo/src/ElTiempo.java
@@ -0,0 +1,15 @@
+//Unidad 1 - actividad 2 - ejercicio 2 - 1.2.2
+
+public class ElTiempo {
+public static void main(String[] args) {
+System.out.println("Hoy hace un día soleado!");
+}
+}
+
+
+
+/*
+*
+*Analizar el error e intentar arreglarlo, una vez arreglado, volver a compilar el código fuente.
+*No tiene ningun error
+*/
