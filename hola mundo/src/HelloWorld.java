9e83a4a0025fe5d48f57af39c3e2cfadbfff8c43
Eduardomorera <morera.e@gmail.com>
2020-11-05 13:04:22 +0100

    //Unidad 1 - actividad 2 - ejercicio 1 - 1.2.1

--- hola mundo/src/HelloWorld.java
+++ hola mundo/src/HelloWorld.java
@@ -1,6 +1,18 @@
 //Unidad 1 - actividad 2 - ejercicio 1 - 1.2.1
-public class HolaMundo {
+public class HelloWorld {
 public static void main(String[] args) {
-System.out.println("Hello world!!");
+//Esto es un comentario!!! Prints "Hello World!" to the terminal window.
+System.out.println("Hello World!");
 }
-} 
+}
+
+
+
+/*
+ * 
+ * ¿Cuál es la finalidad de esta aplicación Java?
+ * La finalidad de esta aplicación es mostrar en una terminal "Hello World!"
+ * 
+ */
+
+
