2d1958a63eaf283cd899b2decffd212b540afbde
Eduardomorera <morera.e@gmail.com>
2020-11-05 12:53:11 +0100

    final

--- hola mundo/src/HolaMundo.java
+++ hola mundo/src/HolaMundo.java
@@ -1,6 +1,6 @@
 //Unidad 1 - actividad 2 - ejercicio 1
 public class HolaMundo {
 public static void main(String[] args) {
-System.out.println("Hello world!! ");
+System.out.println("Hello world!!");
 }
 } 
